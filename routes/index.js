f3ca274ce52a3bb0ca5faed276898d29e573ac03
JmmonJeremy <suc19002@byui.edu>
2024-10-31 09:11:29 -0700

    Added code to connect to a frontend

--- routes/index.js
+++ routes/index.js
@@ -4,39 +4,41 @@ const creation = require('./creation');
 const swagger = require('./swagger');
 const oauth = require('./oauth');
 const oauthCallback = require('./oauth-callback');
+const static = require("./static")
 
 // See https://swagger-autogen.github.io/docs/swagger-2/schemas-and-definitions under @schema section for guidance
-routes.get('/', (req, res) => {
-  /* #swagger.description = "Welcome to the Goal Creation API" */
-  /* #swagger.responses[200] = { 
-        description: "Successfully Loaded Welcome", 
-        '@schema': { 
-             "type": "object",
-            "properties": {
-              "welcome": {
-                "type": "string",
-                "example": "Welcome to the Place that Turns Your Goals into Victorious Creations"
-                } 
-              } 
-            } 
-          } 
-        }    
-  */ 
- try {
-  // res.redirect(
-  //   `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`,
-  // );
-  console.log("GET / route handler is being hit");
-  const docData = 'Welcome to the Place that Turns Your Goals into Victorious Creations!';
-  res.send(docData);
- } catch (error) {
-  res.status(500).send({
-    message:
-      error.message || 'An error occurred while processing the request.',
-  });
- }
-});
+// routes.get('/', (req, res) => {
+//   /* #swagger.description = "Welcome to the Goal Creation API" */
+//   /* #swagger.responses[200] = { 
+//         description: "Successfully Loaded Welcome", 
+//         '@schema': { 
+//              "type": "object",
+//             "properties": {
+//               "welcome": {
+//                 "type": "string",
+//                 "example": "Welcome to the Place that Turns Your Goals into Victorious Creations"
+//                 } 
+//               } 
+//             } 
+//           } 
+//         }    
+//   */ 
+//  try {
+//   // res.redirect(
+//   //   `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`,
+//   // );
+//   console.log("GET / route handler is being hit");
+//   const docData = 'Welcome to the Place that Turns Your Goals into Victorious Creations!';
+//   res.send(docData);
+//  } catch (error) {
+//   res.status(500).send({
+//     message:
+//       error.message || 'An error occurred while processing the request.',
+//   });
+//  }
+// });
 
+routes.use('/', static)
 routes.use('/', oauth)
 routes.use('/', oauthCallback)
 routes.use('/', swagger);
